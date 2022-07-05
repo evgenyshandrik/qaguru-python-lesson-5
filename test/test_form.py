610735945fc0feae6fc64cb25a81d19f6c6f5442
Evgeny Shandrik <yauheni.shandryk@neotech.by>
2022-07-05 15:13:07 +0300

    update test using PO

--- test/test_form.py
+++ test/test_form.py
@@ -1,11 +1,14 @@
 """
 Testing sing up form
 """
-import os
+import time
 
 from selene import have, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
+
+from controls import TagsInput, Dropdown, DatePicker, Table
+from util.get_resources import resource
 
 
 class User():
@@ -17,8 +20,8 @@ class User():
     email = 'test@test.ru'
     sex = 'Male'
     phone = 1234567890
-    date_of_birthday = {'day': '20', 'month': '3', 'year': '1991'}
-    subject = 'English'
+    date_of_birthday = '20.03.1991'
+    subject = ['English', 'Maths']
     hobby = 'Sports'
     address = 'Address street'
     state = 'NCR'
@@ -39,8 +42,11 @@ month_name = {'1': 'January',
               '11': 'November',
               '12': 'December'}
 
-expected_date_of_birthday = f'{User.date_of_birthday.get("day")} ' \
-                            f'{month_name.get(User.date_of_birthday.get("month"))},{User.date_of_birthday.get("year")}'
+year_birthday = int(User.date_of_birthday.split('.')[2])
+month_birthday = int(User.date_of_birthday.split('.')[1])
+day_birthday = int(User.date_of_birthday.split('.')[0])
+expected_date_of_birthday = f'{day_birthday} ' \
+                            f'{month_name.get(str(month_birthday))},{year_birthday}'
 
 
 def open_page():
@@ -48,9 +54,11 @@ def open_page():
     Open(redirect) page contains testing form
     """
     browser.open('/automation-practice-form')
+    time.sleep(1)  # todo: for loading advertise
     (
-        ss('[id^=google_ads][id$=container__]').with_(timeout=10)
-        .should(have.size_greater_than_or_equal(2))
+        browser.all('[id^=google_ads][id$=container__],[id$=Advertisement]')
+        .with_(timeout=10)
+        .should(have.size_greater_than_or_equal(3))
         .perform(command.js.remove)
     )
 
@@ -71,32 +79,38 @@ def test_sign_up():
     mobile_phone = '#userNumber'
     s(mobile_phone).type(User.phone)
 
-    s('#dateOfBirthInput').click()
-    s('.react-datepicker__year-select').element(f'[value="{User.date_of_birthday.get("year")}"]').click()
-    s('.react-datepicker__month-select').element(f'[value="{int(User.date_of_birthday.get("month")) - 1}"]').click()
-    s(f'.react-datepicker__day--0{User.date_of_birthday.get("day")}').click()
+    date_picker = DatePicker(s('#dateOfBirthInput'))
+    date_picker.set_date("20.03.1991")
 
-    s('#subjectsInput').type(User.subject).press_tab()
+    first_user_subject = User.subject[0]
+    second_user_subject = User.subject[1]
+    subjects = TagsInput(s('#subjectsInput'))
+    subjects.add(first_user_subject[0:3], autocomplete=first_user_subject)
+    subjects.add(second_user_subject)
 
     hobbies_types = s('#hobbiesWrapper')
     hobbies_types.all('.custom-checkbox').element_by(have.exact_text(User.hobby)).click()
 
-    s('#uploadPicture').send_keys(os.path.abspath(f'../resources/{User.avatar}'))
+    s('#uploadPicture').send_keys(resource(f'{User.avatar}'))
 
     s('#currentAddress').type(User.address)
-    s('#state').element('input').type(User.state).press_tab()
-    s('#city').element('input').type(User.city).press_tab()
+
+    state_element = Dropdown(s('#state'))
+    state_element.select(option=User.state)
+
+    city_element = Dropdown(s('#city'))
+    city_element.autocomplete(option=User.city)
 
     s('#submit').perform(command.js.click)
 
-    tr = browser.elements("table tr")
-    tr.element(1).should(have.text(f'{User.first_name} {User.last_name}'))
-    tr.element(2).should(have.text(User.email))
-    tr.element(3).should(have.text(User.sex))
-    tr.element(4).should(have.text(str(User.phone)))
-    tr.element(5).should(have.text(expected_date_of_birthday))
-    tr.element(6).should(have.text(User.subject))
-    tr.element(7).should(have.text(User.hobby))
-    tr.element(8).should(have.text(User.avatar))
-    tr.element(9).should(have.text(User.address))
-    tr.element(10).should(have.text(f'{User.state} {User.city}'))
+    result_table = Table(s('.modal-content .table'))
+    result_table.cells_of_row(0).should(have.exact_texts('Student Name', f'{User.first_name} {User.last_name}'))
+    result_table.cells_of_row(1).should(have.exact_texts('Student Email', User.email))
+    result_table.cells_of_row(2).should(have.exact_texts('Gender', User.sex))
+    result_table.cells_of_row(3).should(have.exact_texts('Mobile', str(User.phone)))
+    result_table.cells_of_row(4).should(have.exact_texts('Date of Birth', expected_date_of_birthday))
+    result_table.cells_of_row(5).should(have.exact_texts('Subjects', f'{User.subject[0]}, {User.subject[1]}'))
+    result_table.cells_of_row(6).should(have.exact_texts('Hobbies', User.hobby))
+    result_table.cells_of_row(7).should(have.exact_texts('Picture', User.avatar))
+    result_table.cells_of_row(8).should(have.exact_texts('Address', User.address))
+    result_table.cells_of_row(9).should(have.exact_texts('State and City', f'{User.state} {User.city}'))
