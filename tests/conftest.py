728a56ce2a5e85ca5589a65c1c5f2c47de9ccf7a
Evgeny Shandrik <yauheni.shandryk@neotech.by>
2022-07-21 20:18:50 +0300

    add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ import pytest
 
 from selene import have, command
 from selene.support.shared import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -16,6 +17,17 @@ def browser_management():
     """
     Set up browser
     """
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    browser.config.driver = webdriver.Remote(command_executor="http://user1:1234@selenoid.autotests.cloud:4444/wd/hub",
+                                      desired_capabilities=capabilities)
     browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
     browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
     browser.config.hold_browser_open = (
